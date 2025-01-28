d7bc09255050b70021bd0014e02ae3d983df3df1
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2025-01-28 22:24:43 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import createVersionPlugin from './plugin/vite-plugin-version/index'; // Adjust the path as needed
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), createVersionPlugin()],
+  server: {
+    hmr: {overlay:true},
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
